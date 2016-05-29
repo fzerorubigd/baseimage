85a8f80d2cdd214d218ea5795ae75c473df592e6
fzerorubigd <fzerorubigd@gmail.com>
2016-05-29 23:14:09 +0430

    add vagrant related things

--- image/helium.sh
+++ image/helium.sh
@@ -3,8 +3,31 @@ set -euo pipefail
 source /bd_build/buildconfig
 
 apt-get update
-$minimal_apt_get_install git zsh nano libsqlite3-dev autoconf bison build-essential libssl-dev \
+$minimal_apt_get_install wget curl sudo git zsh nano libsqlite3-dev autoconf bison build-essential libssl-dev \
                 libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev htop redis-server postgresql mercurial \
                 ruby-dev rabbitmq-server realpath pkg-config unzip dnsutils re2c python-pip \
                 python-dev libpq-dev tmux bzr libsodium-dev cmake default-jdk golang
 
+
+
+GOBIN=/usr/local/bin GOPATH=/tmp go get -v -u github.com/mailhog/MailHog
+cd /tmp
+git clone https://github.com/fzerorubigd/tmass.git
+cd tmass
+make all
+cp bin/tmass /usr/local/bin
+
+# Create vagrant user
+bash -c "echo root:bita123 | chpasswd"
+groupadd develop
+useradd -g develop -m -s /usr/bin/zsh develop
+bash -c "echo %develop ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant"
+chmod 0440 /etc/sudoers.d/vagrant
+bash -c "echo develop:bita123 | chpasswd"
+sudo -Hu develop -- wget -O /home/develop/.zshrc http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+sudo -Hu develop -- wget -O /home/develop/.zshrc.local  http://git.grml.org/f/grml-etc-core/etc/skel/.zshrc
+
+# Go related stuff
+bash -c "echo export GOPATH=/home/develop >> /etc/environment"
+sudo -Hu develop -- bash -c "echo export GOPATH=/home/develop >> /home/develop/.zshrc.local"
+sudo -Hu develop -- bash -c "echo export PATH=$PATH:/usr/local/go/bin:/home/develop/bin >> /home/develop/.zshrc.local"
